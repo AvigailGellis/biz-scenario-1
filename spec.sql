7a285e186f9e91a50648530ab6da1b6af5a7d1e5
brachagreenberg <92119470+brachagreenberg@users.noreply.github.com>
2023-01-11 13:37:44 -0500

    Update spec.sql

--- spec.sql
+++ spec.sql
@@ -2,14 +2,11 @@
 I am a Personal Trainer and I work with clients to build their bodies and coach them how to lose 
 weight from the correct excersizes.
 I have many clients and I need a system to keep track of them - their First and Last Names, 
-home address, phone number, starting weight, current weight, height, BMI (body mass index), date that
+home address, phone number, starting weight, current weight, height, date that
 they signed up, date of last session, date of upcoming session, balance owed
 
 Q: What unit of measure do you use?
 A: pounds for weight and inches for height
-
-Q:What is BMI and how is it calculated?
-A:???
 
 Q:Is there a max balance owed? Is there a number that if reached, you may no longer book further
 sessions until paid up?
@@ -59,16 +56,16 @@ hear raving reviews from their friends, sisters, and neighbors and any  remainin
 answered by visiting our website.
 
 Sample Data - Current Clients
-Esti Anteby, 120 Rock Hill Road Monsey NY 1092, 8455258521, 120, 116, 61, ???,  July 17 2020, September 9 2022, November 1 2022, $125,
-Batsheva Cohen, 53 Regina Road Monsey NY 10952, 8456422919, 137, 136, 66.5, ???, July 18 2017, September 10 2022, November 2 2022, $275,
-Peri Felder, 10 Laurel Park Road Fallsburg NY 12733, 8482264678, 114, 110, 65.5, ???, March 5 2007, September 11 2022, November 3 2022, $250,
-Bashi Shapiro, 143 Ridge Avenue Lakewood NJ 08701, 8455402981, 136, 126, 64, ???, August 7 2017, September 12 2022, November 2 2022, $75,
-Ruchi Katz, 12 Boxwood Lane Monsey NY 10952, 8455020634, 124, 120, 62, ???, October 12 2015, September 13 2022, November 12 2022, $0,
-Rena Gawartin, 1432 Laurelwood Avenue Lakewood NJ 08701, 8453673278, 107, 107, 60, ???, April 27 2018, September 14 2022, November 20 2022, $0
-Hinda Fiddle, 240 Horizon Court Monsey NY 10952, 8456644581, 143, 137, 61, ???, April 3 2019, September 15 2022, November 7 2022, $25,
-Chaya Littman, 1 Brickman Road Fallsburg NY 12733, 8454282847, 119, 116, 60, ???, January 13 2015, September 16 2022, November 9 2022, $0,
-Shana Jacoby, 9 Park Avenue Passaic NJ 07055, 5517957446, 131, 127, 65, ???, September 9 2016, September 17 2022, November 6 2022, $75
-Cheved Sacks, 89 Howard Avenue Passaic NJ 07055, 9734897654, 140,138, 67, ???, July 17 2009, September 18 2022, November 14 2022, $0
+Esti Anteby, 120 Rock Hill Road Monsey NY 1092, 8455258521, 120, 116, 61, July 17 2020, September 9 2022, November 1 2022, $125,
+Batsheva Cohen, 53 Regina Road Monsey NY 10952, 8456422919, 137, 136, 66.5, July 18 2017, September 10 2022, November 2 2022, $275,
+Peri Felder, 10 Laurel Park Road Fallsburg NY 12733, 8482264678, 114, 110, 65.5, March 5 2007, September 11 2022, November 3 2022, $250,
+Bashi Shapiro, 143 Ridge Avenue Lakewood NJ 08701, 8455402981, 136, 126, 64, August 7 2017, September 12 2022, November 2 2022, $75,
+Ruchi Katz, 12 Boxwood Lane Monsey NY 10952, 8455020634, 124, 120, 62, October 12 2015, September 13 2022, November 12 2022, $0,
+Rena Gawartin, 1432 Laurelwood Avenue Lakewood NJ 08701, 8453673278, 107, 107, 60, April 27 2018, September 14 2022, November 20 2022, $0
+Hinda Fiddle, 240 Horizon Court Monsey NY 10952, 8456644581, 143, 137, 61, April 3 2019, September 15 2022, November 7 2022, $25,
+Chaya Littman, 1 Brickman Road Fallsburg NY 12733, 8454282847, 119, 116, 60, January 13 2015, September 16 2022, November 9 2022, $0,
+Shana Jacoby, 9 Park Avenue Passaic NJ 07055, 5517957446, 131, 127, 65, September 9 2016, September 17 2022, November 6 2022, $75
+Cheved Sacks, 89 Howard Avenue Passaic NJ 07055, 9734897654, 140,138, 67, July 17 2009, September 18 2022, November 14 2022, $0
 
 Reports
 I want to see a list of all my current clients presented as follows:
